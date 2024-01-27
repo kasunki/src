47e8e8f9671f7abc29ce95be661dc7710256f7f2
kasunki <kasunki.samarasekara@student.hamk.fi>
2024-01-27 16:03:58 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <!-- this is html comment -->
+
+<?php
+
+// this is printing hello World
+# another single line comment
+/*multiple
+line comments
+
+*/
+
+ echo "<h1>Hello World</h1>";
+ ?>
+
+
+
+</body>
+</html>
+
+
+
