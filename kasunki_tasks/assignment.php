190a720537c2df9306e42c53798333fbe9d4330a
kasunki <kasunki.samarasekara@student.hamk.fi>
2024-01-29 19:11:46 +0200

    exercise 2 done

---
+++ kasunki_tasks/assignment.php
@@ -0,0 +1,10 @@
+
+              <?php 
+              $title = "Assignment"; 
+              
+              include 'header.php' ?>
+
+              <img class="img-fluid" src="images/rose.jpg">
+
+              <?php include 'footer.php' ?>
+            
---
+++ kasunki_tasks/contact.php
@@ -0,0 +1,20 @@
+ <?php
+    $title = "Contact"; 
+    include 'header.php' ?>
+    <h1>My Contact Information</h1>
+
+    <h2>Address</h2>
+
+    <!-- br creates a line break -->
+
+    <p>
+        Kasunki Samarasekara <br>
+        Esikkotie 9C
+        Vantaa
+    </p>
+
+    <h2>Email</h2>
+
+    <p>kasunkie@gmail.com</p>
+
+    <?php include 'footer.php' ?>
