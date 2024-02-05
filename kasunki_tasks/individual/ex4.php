f9cc278f419e59dd456871b3cbe756a292b79996
kasunki <kasunki.samarasekara@student.hamk.fi>
2024-02-05 19:30:40 +0200

    typo

--- kasunki_tasks/individual/ex4.php
+++ kasunki_tasks/individual/ex4.php
@@ -18,7 +18,7 @@
             $age =$_POST["userAge"];
 
         if ($age >=18){
-            echo "You are eligle for voting.";
+            echo "You are eligible for voting.";
         }
         else {
             echo "You are not eligible for voting.";
